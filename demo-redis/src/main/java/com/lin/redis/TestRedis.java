1211538baba009ade8ec512d9e2a81f5b3c5a2d7
林嘉宝 <linjb@signalway.com.cn>
2018-09-26 17:18:52 +0800

    merge

---
+++ demo-redis/src/main/java/com/lin/redis/TestRedis.java
@@ -0,0 +1,21 @@
+package com.lin.redis;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author: 林嘉宝
+ *  
+ * @Date: 2018/9/22
+ *  
+ * @name: 
+ *
+ * @Description: 
+ */
+public class TestRedis {
+
+    public static void main(String[] args) {
+        LinkedHashMap map = new LinkedHashMap();
+
+    }
+
+}
