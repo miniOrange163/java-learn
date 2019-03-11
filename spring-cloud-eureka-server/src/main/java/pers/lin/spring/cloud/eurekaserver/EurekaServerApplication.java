89f951ef2c65a9a80ab33212b0061296fd91b0a4
miniOrange163 <261780401@qq.com>
2019-03-12 01:27:30 +0800

    spring boot admin 模块：调试中

--- spring-cloud-eureka-server/src/main/java/pers/lin/spring/cloud/eurekaserver/EurekaServerApplication.java
+++ spring-cloud-eureka-server/src/main/java/pers/lin/spring/cloud/eurekaserver/EurekaServerApplication.java
@@ -3,6 +3,9 @@ package pers.lin.spring.cloud.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
--- spring-cloud-service-multiply/src/main/java/pers/lin/spring/cloud/service/operation/MultiplyOperationApplication.java
+++ spring-cloud-service-multiply/src/main/java/pers/lin/spring/cloud/service/operation/MultiplyOperationApplication.java
@@ -3,6 +3,9 @@ package pers.lin.spring.cloud.service.operation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +15,13 @@ public class MultiplyOperationApplication {
         SpringApplication.run(MultiplyOperationApplication.class, args);
     }
 
+//    @Configuration
+//    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http.authorizeRequests().anyRequest().permitAll()
+//                    .and().csrf().disable();
+//        }
+//    }
 }
 
